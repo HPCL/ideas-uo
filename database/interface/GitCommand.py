7e7b59b3d95ed0b33a8842bba49a68aa6938aa32
Carter Perkins <cartersperkins@gmail.com>
2021-02-01 19:50:54 -0800

    db update script changes

--- database/interface/GitCommand.py
+++ database/interface/GitCommand.py
@@ -37,7 +37,8 @@ class GitCommand(object):
             return '',getYears(reponame)
         return 'tags/',[x.strip() for x in open(filepath,'r').readlines()]
 
-    #Get all the versions of a repo since data (default: utc epoch)
+
+    #Get all commits in all the versions of a repo since data (default: utc epoch) and group by author
     def getRepoCommitData(self, reponame, since=datetime.datetime.utcfromtimestamp(0).isoformat()):
 
         prefix,versions = self.getRepoVersions(reponame)
@@ -52,7 +53,8 @@ class GitCommand(object):
 
 
         #git log -p # this will list all commits and the code additions in addition to dates and messages.
-        retcode, out, err = Command.Command(f'git log -p --since={since}').run()
+        # function-context for python just adds all the surrounding lines of code to the diff output
+        retcode, out, err = Command.Command(f'git log -p --date=iso-strict-local --function-context --since={since}').run()
         lines = iter(out.splitlines())
 
         current_author = ''
@@ -64,6 +66,10 @@ class GitCommand(object):
             if line.startswith(b'commit'):
 
                 commitid = line[7:len(line)]
+
+                #Retrieve all branches that contains this commit
+                #git branch -a --contains <commit>
+                retcode, branches, err = Command.Command('git branch -a --contains %s' % commitid).run()
 
                 line = next(lines)
                 #print(line)
@@ -110,14 +116,23 @@ class GitCommand(object):
                             #print('FILENAME '+ filename)
 
                             line = next(lines)
+
                             #skip extra line if this line is seen
                             if line.startswith(b'new file mode'):
                                 next(lines)
+                                diff = next(lines)
+                                if diff.startswith(b'diff'):
+                                    break
 
                             if not line.startswith(b'deleted file mode') and not line.startswith(b'old mode'):
 
-                                next(lines)
-                                next(lines)
+                                #skip just one line if this line is seen
+                                if line.startswith(b'new file mode'):
+                                    next(lines)
+
+                                else:
+                                    next(lines)
+                                    next(lines)
 
                                 #skip ahead until see first + or -
                                 diff = next(lines)
@@ -157,7 +172,7 @@ class GitCommand(object):
 
                     #print(diffs)
                     #add the commit to the author's list of commits.
-                    commits[current_author]['commits'].append({'id':commitid, 'date':date,'message':message, 'diffs':diffs})
+                    commits[current_author]['commits'].append({'id':commitid, 'date':date, 'message':message, 'diffs':diffs, 'branches':branches})
 
                 elif line.startswith(b'Merge: '):
                     #ignore merges
@@ -170,7 +185,142 @@ class GitCommand(object):
         return commits
 
 
+    #Get all commits in all versions of repo and put in flat list
+    def getAllCommits(self, reponame):
 
+        prefix,versions = self.getRepoVersions(reponame)
+
+        commits = []
+
+        for version in versions:
+            #checkout the versions
+            print('git checkout %s%s' % (prefix,version))
+            retcode, out, err = Command.Command('git checkout %s%s' % (prefix,version)).run(dryrun=False)
+            print(out)
+
+
+        #git log -p # this will list all commits and the code additions in addition to dates and messages.
+        # function-context for python just adds all the surrounding lines of code to the diff output
+        retcode, out, err = Command.Command('git log -p --date=iso-strict-local --function-context').run()
+        lines = iter(out.splitlines())
+
+        #current_author = ''
+        for line in lines:
+
+
+            #If the line is an author, then start to piece together the commit
+            if line.startswith(b'commit'):
+
+                commitid = line[7:len(line)]
+
+                #Retrieve all branches that contains this commit
+                #git branch -a --contains <commit>
+                retcode, branches, err = Command.Command('git branch -a --contains %s' % commitid).run()
+
+                line = next(lines)
+                #print(line)
+
+                if line.startswith(b'Author: '):
+
+                    #get the commit date
+                    rawdate = next(lines)
+                    date = rawdate[8:len(rawdate)]
+
+                    #print(rawdate)
+
+                    #get the commit message
+                    next(lines)
+                    message = ''
+                    m = next(lines)
+                    #print(m)
+                    while len(m) > 1:
+                        message += (m + b'\n').decode("utf-8")
+                        try:
+                            m = next(lines)
+                        except StopIteration:
+                            m = ''
+
+                    #get the diffs
+                    #this code will iterate over the lines trying to pull just the +/- info from the diff output
+                    diffs = []
+                    diff = next(lines)
+                    while len(diff) > 0 and diff.startswith(b'\\') == False:
+                        if diff.startswith(b'diff'):
+                            #next(lines)
+                            #next(lines)
+                            #next(lines)
+                            filenameline = diff.decode("utf-8")
+                            filename = filenameline[11:len(filenameline)]
+                            #print('FILENAME '+ filename)
+
+                            line = next(lines)
+
+                            #skip extra line if this line is seen
+                            if line.startswith(b'new file mode'):
+                                next(lines)
+                                diff = next(lines)
+                                if diff.startswith(b'diff'):
+                                    break
+
+                            if not line.startswith(b'deleted file mode') and not line.startswith(b'old mode'):
+
+                                #skip just one line if this line is seen
+                                if line.startswith(b'new file mode'):
+                                    next(lines)
+
+                                else:
+                                    next(lines)
+                                    next(lines)
+
+                                #skip ahead until see first + or -
+                                diff = next(lines)
+                                while not (diff.startswith(b'+') or diff.startswith(b'-')) or (diff.startswith(b'+++') or diff.startswith(b'---')) :
+                                    diff = next(lines)
+
+                                diffinfo = []
+                                while len(diff) >= 1:
+
+                                    if (diff.startswith(b'+') or diff.startswith(b'-')):
+                                        diffinfo.append(diff.decode("utf-8", errors='ignore'))
+
+                                    elif diff.startswith(b'diff'):
+                                        break
+                                    elif len(diff) < 2:
+                                        try:
+                                            diff = next(lines)
+                                            if len(diff) < 2:
+                                                break
+                                        except:
+                                            break
+
+                                    try:
+                                        diff = next(lines)
+                                    except:
+                                        break
+
+                                diffs.append({'filename':filename, 'diff':diffinfo})
+
+                            #else:
+                                #ignore deleted files for now
+                        else:
+                            try:
+                                diff = next(lines)
+                            except:
+                                break
+
+                    #print(diffs)
+                    #add the commit to the author's list of commits.
+                    commits.append({'id':commitid, 'date':date, 'message':message, 'diffs':diffs, 'branches':branches})
+
+                elif line.startswith(b'Merge: '):
+                    #ignore merges
+                    next(lines)
+
+            #else:
+                #ignore anything else until next author line
+
+        #print(commits)
+        return commits
 
 
 #Helper Functions
