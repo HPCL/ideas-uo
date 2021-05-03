66f17036c67d7feff9d4604b871e5a5b9ddfa4d7
Carter Perkins <cartersperkins@gmail.com>
2021-05-02 23:16:17 -0700

    graphql progress

--- src/gitutils/graphql_interface.py
+++ src/gitutils/graphql_interface.py
@@ -1,3 +1,4 @@
+import configparser
 import enum
 import logging
 import json
@@ -5,8 +6,10 @@ import requests
 
 from src.gitutils.graphql_queries import *
 
-GITHUB_TOKEN = 'secret'
-GITLAB_TOKEN = 'secret'
+config = configparser.ConfigParser()
+config.read('credentials.ini')
+GITHUB_TOKEN = config.get('github', 'token')
+GITLAB_TOKEN = config.get('gitlab', 'token')
 
 class Source(enum.Enum):
     GITHUB = {'url': 'https://api.github.com/graphql',
@@ -20,34 +23,116 @@ def fetch_issues(owner, repo, source):
         raise Exception(f'Unknown source: {source}')
 
     payload = source.value
-    issue = GITHUB_ISSUE if source == Source.GITHUB else GITLAB_ISSUE
+    query = GITHUB_ISSUE if source == Source.GITHUB else GITLAB_ISSUE
     stage = 'repository' if source == Source.GITHUB else 'project'
 
     import time
     cursor = None
+    print(f'Fetching issues from {source.name}...')
+    issues = []
     start = time.time()
     while True:
-        payload['json'] = {'query': issue % (owner, repo),
+        payload['json'] = {'query': query % (owner, repo),
                        'variables': {'cursor': cursor}}
         r = requests.post(**payload)
         j = r.json()
-        entry = j['data'][stage]['issues']
-        pagination = entry['pageInfo']
-        if source == Source.GITHUB:
-            print(entry['totalCount'])
-        else:
-            print(entry['count'])
-        print(entry['nodes'].__len__())
-        for node in entry['nodes']:
-            print(node)
+        try:
+            entry = j['data'][stage]['issues']
+            #print(j)
+        except:
+            print(j)
+            print('Connection error with GraphQL')
             break
-        break
+        pagination = entry['pageInfo']
+
+        for node in entry['nodes']:
+            issue = {}
+            issue['title'] = node['title']
+            issue['description'] = node['description'] if source == Source.GITLAB else node['body']
+            issue['updatedAt'] = node['updatedAt']
+            issue['closedAt'] = node['closedAt']
+            issue['locked'] = node['discussionLocked'] if source == Source.GITLAB else node['locked']
+            issue['url'] = node['webUrl'] if source == Source.GITLAB else node['url']
+            issue['number'] = node['iid'] if source == Source.GITLAB else node['number']
+
+            try:
+                username = node['author']['username'] if source == Source.GITLAB else node['author']['login']
+            except:
+                username = None
+
+            try:
+                url = node['author']['webUrl'] if source == Source.GITLAB else node['author']['url']
+            except:
+                url = None
+
+            issue['author'] = {
+                'username': username,
+                'url': url
+            }
+
+            def collect_assignee(node):
+                return {
+                    'name': node['name'],
+                    'username': node['username'] if source == Source.GITLAB else node['login'],
+                    'email': node['publicEmail'] if source == Source.GITLAB else node['email'],
+                    'url': node['webUrl'] if source == Source.GITLAB else node['url']
+                }
+
+            issue['assignees'] = [collect_assignee(assignee) for assignee in node['assignees']['nodes']]
+            issue['milestone'] = None if not node['milestone'] else {
+                'state': node['milestone']['state'],
+                'description': node['milestone']['description'],
+                'title': node['milestone']['title'],
+                'dueOn': node['milestone']['dueDate'] if source == Source.GITLAB else node['milestone']['dueOn'],
+                'createdAt': node['milestone']['createdAt'],
+                'updatedAt': node['milestone']['updatedAt']
+            }
+
+            def collect_label(node):
+                return {
+                    'name': node['title'] if source == Source.GITLAB else node['name']
+                }
+
+            issue['labels'] = [collect_label(label) for label in node['labels']['nodes']]
+
+            def collect_comment(node):
+                try:
+                    username = node['author']['username'] if source == Source.GITLAB else node['author']['login']
+                except:
+                    username = None
+
+                try:
+                    url = node['author']['webUrl'] if source == Source.GITLAB else node['author']['url']
+                except:
+                    url = None
+
+                return {
+                    'author': {
+                        'username': username,
+                        'url': url
+                    },
+                    'createdAt': node['createdAt'],
+                    'updatedAt': node['updatedAt'],
+                    'url': node['url'],
+                    'id': node['id'],
+                    'body': node['body']
+                }
+
+            centry = 'notes' if source == Source.GITLAB else 'comments'
+            issue['comments'] = [collect_comment(comment) for comment in node[centry]['nodes'] if comment]
+
+            issues.append(issue)
+        print('.', end='')
+
         cursor = pagination['endCursor']
         if not pagination['hasNextPage']:
             break
 
+    print()
     end = time.time()
     print(f'Done in {end - start:.2f}s.')
+    print(f'Got {len(issues)} issues.')
+    return issues
 
 fetch_issues('petsc', 'petsc', Source.GITLAB)
-fetch_issues('pytorch', 'pytorch', Source.GITHUB)
+fetch_issues('google', 'gvisor', Source.GITHUB)
--- src/gitutils/graphql_queries.py
+++ src/gitutils/graphql_queries.py
@@ -7,6 +7,84 @@ GITHUB_ISSUE = '''
                     title
                     createdAt
                     updatedAt
+                    closedAt
+                    state
+                    locked
+                    url
+                    body
+                    number
+                    assignees(first: 100) {
+                        nodes {
+                            name
+                            login
+                            email
+                            url
+                        }
+                    }
+                    author {
+                        login
+                        url
+                    }
+                    milestone {
+                        state
+                        description
+                        title
+                        dueOn
+                        createdAt
+                        updatedAt
+                    }
+                    labels(first: 100) {
+                        nodes {
+                            name
+                        }
+                    }
+                    comments(first: 100) {
+                        nodes {
+                            author {
+                                login
+                                url
+                            }
+                            body
+                            createdAt
+                            updatedAt
+                            url
+                            id
+                        }
+                    }
+                }
+                pageInfo {
+                    endCursor
+                    hasNextPage
+                }
+            }
+        }
+        rateLimit {
+            limit
+            cost
+            remaining
+            resetAt
+        }
+    }
+    '''
+
+GITHUB_PULLREQUEST = '''
+    query($cursor: String) {
+        repository(owner: "%s" name: "%s") {
+            pullRequests(first: 100, after: $cursor) {
+                totalCount
+                nodes {
+                    headRefOid
+                    commits(first: 100) {
+                        nodes {
+                            commit {
+                                oid
+                            }
+                        }
+                    }
+                    title
+                    createdAt
+                    updatedAt
+                    mergedAt
                     state
                     locked
                     url
@@ -58,7 +136,7 @@ GITHUB_ISSUE = '''
             }
         }
     }
-    '''
+'''
 
 GITLAB_ISSUE = '''
     query($cursor: String) {
@@ -66,11 +144,79 @@ GITLAB_ISSUE = '''
             issues(first: 100000000, after: $cursor) {
                 count
                 nodes {
+                    description
                     title
                     updatedAt
                     closedAt
                     discussionLocked
                     webUrl
+                    iid
+                    assignees {
+                        nodes {
+                            name
+                            username
+                            publicEmail
+                            webUrl
+                        }
+                    }
+                    author {
+                        username
+                        webUrl
+                    }
+                    milestone {
+                        state
+                        description
+                        createdAt
+                        updatedAt
+                        title
+                        dueDate
+                    }
+                    labels {
+                        nodes {
+                            title
+                        }
+                    }
+                    notes {
+                        nodes {
+                            author {
+                                username
+                                webUrl
+                            }
+                            body
+                            createdAt
+                            updatedAt
+                            url
+                            id
+                        }
+                    }
+                }
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+            }
+        }
+    }
+'''
+
+GITLAB_PULLREQUEST = '''
+    query($cursor: String) {
+        project(fullPath: "%s/%s") {
+            mergeRequests(first: 100000000, after: $cursor) {
+                count
+                nodes {
+                    commitsWithoutMergeCommits {
+                        nodes {
+                            sha
+                        }
+                    }
+                    diffHeadSha
+                    title
+                    updatedAt
+                    mergedAt
+                    discussionLocked
+                    webUrl
+                    description
                     iid
                     assignees {
                         nodes {
