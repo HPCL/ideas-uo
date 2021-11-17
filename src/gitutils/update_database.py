93e4badc0ce47ac3771fff1cd01b72c896c43d06
Jason Prideaux <jprideau@cs.uoregon.edu>
2021-11-17 08:48:51 -0800

    Can update just one project with new param.

--- src/gitutils/update_database.py
+++ src/gitutils/update_database.py
@@ -12,10 +12,14 @@ PASSWORD = '' # edit this
 PORT = 3331
 DATABASE = 'ideas_db'
 LOG_DIR = '/shared/soft/ideas_db/logs/'
+PROJECT = 0
 
 if len(sys.argv)>2: 
     USERNAME=sys.argv[1]
     PASSWORD=sys.argv[2]
+
+if len(sys.argv)>3:
+    PROJECT=int(sys.argv[3])
 
 def run(cmd: str, dry_run=True):
     print(cmd)
@@ -23,7 +27,7 @@ def run(cmd: str, dry_run=True):
         os.system(cmd)
 
 # Pass in a project id to update only that project
-def update(project=0):
+def update():
     db = MySQLdb.connect(host=HOST,
                          port=PORT,
                          user=USERNAME,
@@ -43,7 +47,7 @@ def update(project=0):
     all_commands = ''
     count = 0
     for name, source_url, id in project_info:
-        if project <= 0 or project == id:
+        if PROJECT <= 0 or PROJECT == id:
             # Python -- 3.9 preferred
             # Add git info
             # This can take a long time, but is not subject to any API limits, so several projects can be updated simultaneously
@@ -57,7 +61,7 @@ def update(project=0):
 
     all_commands = ''
     for name, source_url, id in project_info:
-        if project <= 0 or project == id:
+        if PROJECT <= 0 or PROJECT == id:
             # The updates below are subject to the GraphQL API limits (5000 points per hour), so these are run in order
             # Add prs
             pr_log_path = os.path.join(LOG_DIR, f'{name}_pr.log')
