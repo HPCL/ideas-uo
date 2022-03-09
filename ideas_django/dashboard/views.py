cb3431fa5fd2943baba86d6d8108f34e462680c1
Jason Prideaux <jprideau@cs.uoregon.edu>
2022-03-09 12:17:42 -0800

    Project page dev.

--- ideas_django/dashboard/views.py
+++ ideas_django/dashboard/views.py
@@ -57,7 +57,18 @@ def project(request, *args, **kwargs):
     prs = list(PullRequest.objects.all().filter(project=project).all())
     prs = sorted(prs, key=lambda d: d.number, reverse=True)
 
-    context = {'project':project,'prs':prs}
+    commits = list(Commit.objects.all().filter(project=project).all())
+    commits = sorted(commits, key=lambda d: d.datetime, reverse=True)
+
+    issues = list(Issue.objects.all().filter(project=project).all())
+    issues = sorted(issues, key=lambda d: d.number, reverse=True)
+
+
+    loc = countlines(r'../ideas-uo/'+project.name)
+    files = countfiles(r'../ideas-uo/'+project.name)
+
+
+    context = {'project':project,'prs':prs, 'commits':commits, 'issues':issues, 'loc':loc, 'files':files}
 
     return HttpResponse(template.render(context, request))
 
@@ -356,3 +367,53 @@ def branchData(request):
     	json.dumps(resultdata),
     	content_type='application/json'
     )
+
+
+def countlines(start, lines=0, header=True, begin_start=None):
+    #if header:
+    #    print('{:>10} |{:>10} | {:<20}'.format('ADDED', 'TOTAL', 'FILE'))
+    #    print('{:->11}|{:->11}|{:->20}'.format('', '', ''))
+
+    # TODO: Currently only counds python code.
+
+    for thing in os.listdir(start):
+        thing = os.path.join(start, thing)
+        if os.path.isfile(thing):
+            if thing.endswith('.py'):
+                with open(thing, 'r') as f:
+                    newlines = f.readlines()
+                    newlines = len(newlines)
+                    lines += newlines
+
+                    #if begin_start is not None:
+                    #    reldir_of_thing = '.' + thing.replace(begin_start, '')
+                    #else:
+                    #    reldir_of_thing = '.' + thing.replace(start, '')
+
+                    #print('{:>10} |{:>10} | {:<20}'.format(newlines, lines, reldir_of_thing))
+
+
+    for thing in os.listdir(start):
+        thing = os.path.join(start, thing)
+        if os.path.isdir(thing):
+            lines = countlines(thing, lines, header=False, begin_start=start)
+
+    return lines
+
+
+def countfiles(start, files=0, header=True, begin_start=None):
+
+    # TODO: Counts all files
+
+    for thing in os.listdir(start):
+        thing = os.path.join(start, thing)
+        if os.path.isfile(thing):
+            files += 1
+
+    for thing in os.listdir(start):
+        if not thing.startswith('.git'):
+            thing = os.path.join(start, thing)
+            if os.path.isdir(thing):
+                files = countfiles(thing, files, header=False, begin_start=start)
+
+    return files    
