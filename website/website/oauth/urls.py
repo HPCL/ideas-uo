08c54e4c03ef033b1fc677b76f263751a08de6c8
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-04-18 16:06:46 -0700

    Setup Django static files (js only)

--- website/website/oauth/urls.py
+++ website/website/oauth/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
