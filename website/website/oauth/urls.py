c0b31173e7d60315d1731ad2f8a4c6c8d6c080ba
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-05-16 16:23:13 -0700

    Implement API call to get all user repos

--- website/website/oauth/urls.py
+++ website/website/oauth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('user', views.user, name='user'),
-  path('authorize', views.authorize, name='authorize'),
+    path('user', views.user, name='user'),
+    path('authorize', views.authorize, name='authorize'),
 ]
