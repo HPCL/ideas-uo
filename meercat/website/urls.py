ebdfc5a77e7f66eb6c69e40d65e3247aad10e7ee
Jason Prideaux <jprideau@cs.uoregon.edu>
2022-06-03 14:58:51 -0700

    Syntax, bug fix.

--- meercat/website/urls.py
+++ meercat/website/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('oauth/', include('oauth.urls')),
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='oauth/login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='oauth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='oauth/logout.html'), name='logout')
 ]
