ca75f8638ccca5ae6447fef12eed78172b52c196
Jason Prideaux <jprideau@cs.uoregon.edu>
2022-06-03 15:39:38 -0700

    Fixing more settings bugs.

--- meercat/website/settings.py
+++ meercat/website/settings.py
@@ -127,12 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/dist/'
+#STATIC_URL = 'static/dist/'
 
-# STATIC_ROOT = BASE_DIR / 'website/static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 
-# ]
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
