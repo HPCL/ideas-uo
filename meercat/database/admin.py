5df098bd25be5777d6f789772407522a5dd6481f
Jason Prideaux <jprideau@cs.uoregon.edu>
2022-06-06 12:40:46 -0700

    Missing import of project role.

--- meercat/database/admin.py
+++ meercat/database/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Author, Project, ProjectAuthor, Commit, Diff, Issue, PullRequest, Comment
+from .models import Author, Project, ProjectAuthor, Commit, Diff, Issue, PullRequest, Comment, ProjectRole
  
 # Register your models here.
 @admin.register(Author)
