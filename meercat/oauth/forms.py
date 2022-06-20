f374c5a95498764f2cbc0d2cedfba959b89ba9d0
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-06-20 09:49:53 -0700

    Add personal email, gh username to registration form

--- meercat/oauth/forms.py
+++ meercat/oauth/forms.py
@@ -6,6 +6,8 @@ class RegistrationForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField(label='GitHub account email', max_length=200)
     message = forms.CharField()
+    username = forms.CharField(max_length=100)
+    personal_email = forms.EmailField(label='Personal email', max_length=200, required=False)
 
     #set field attributes
     name.widget = forms.TextInput(attrs={'class': 'form-control'})
@@ -13,9 +15,11 @@ class RegistrationForm(forms.Form):
     message.widget = forms.Textarea(
         attrs={
             'class': 'form-control',
-            'placeholder': 'Please tell us which repositories you would like to join, and your role in them.'
+            'placeholder': 'Please tell us about your interest in MeerCAT.'
         }
     )
+    username.widget = forms.TextInput(attrs={'class': 'form-control'})
+    personal_email.widget = forms.EmailInput(attrs={'class': 'form-control'})
 
     #Unique email constraint
     def clean_email(self):
