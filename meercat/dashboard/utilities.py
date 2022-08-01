6da0551c2f4f1a8333368975e3d84909071acea2
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-08-01 14:36:52 -0700

    Add function that gets branches and status for a project

--- meercat/dashboard/utilities.py
+++ meercat/dashboard/utilities.py
@@ -1,5 +1,9 @@
+import requests
+
 from database.models import EventLog
+from database.utilities import get_repo_owner
 from datetime import datetime
+
 
 def save_debug_event(log, json='', pull_request=None, request=None):
 
@@ -26,3 +30,32 @@ def save_debug_event(log, json='', pull_request=None, request=None):
         pull_request=pull_request
     )
     debug_event.save()
+
+
+def get_branches_with_status(project): # only works with public repos for now
+    repo_owner = get_repo_owner(project)
+    repo_name = project.name
+
+    pulls_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls?state=all"
+    branches_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/branches"
+
+    pulls = requests.get(pulls_url).json()
+    branches = requests.get(branches_url).json()
+
+    branch_names = [branch['name'] for branch in branches]
+    processed_branches = []
+    branches_with_status = []
+
+    # Process branches with pull request
+    for pr in pulls:
+        branch_name = pr['head']['ref']
+        if branch_name in branch_names and branch_name not in processed_branches:
+            branches_with_status.append({'branch': branch_name, 'status': pr['state']})
+            processed_branches.append(branch_name)
+
+    # Process branches without pull request
+    for branch_name in branch_names:
+        if branch_name not in processed_branches:
+            branches_with_status.append({'branch': branch_name, 'status': 'No Pull Request'})
+
+    return branches_with_status
