872b33ee66bd85f41b52e543b8cb8ad1ee459596
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-08-01 12:42:17 -0700

    Improve save_debug_event interface

--- meercat/dashboard/utilities.py
+++ meercat/dashboard/utilities.py
@@ -1,7 +1,19 @@
 from database.models import EventLog
 from datetime import datetime
 
-def save_debug_event(log, json='', pull_request=None, user=None, uri='', view_name=''):
+def save_debug_event(log, json='', pull_request=None, request=None):
+
+    if request is not None:
+        uri = request.build_absolute_uri()
+        view_name = request.resolver_match.view_name
+        if request.user.is_anonymous:
+            user = None
+        else:
+            user = request.user
+    else:
+        uri = ''
+        view_name = ''
+        user = None
 
     debug_event = EventLog(
         user=user,
