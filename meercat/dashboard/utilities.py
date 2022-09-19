931fbad3d1ca417686ab66db01500efd77e6552f
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-09-19 14:41:28 -0700

    Add gmail_send_message function

--- meercat/dashboard/utilities.py
+++ meercat/dashboard/utilities.py
@@ -1,4 +1,6 @@
 import ast
+import os
+from msilib.schema import Error
 import re
 import requests
 from subprocess import check_output
@@ -6,8 +8,8 @@ from datetime import datetime
 from pathlib import Path
 
 from django.conf import settings
-from database.models import EventLog
-from database.utilities import get_repo_owner
+# from database.models import EventLog
+# from database.utilities import get_repo_owner
 
 
 def save_debug_event(log, json='', pull_request=None, request=None):
@@ -184,3 +186,69 @@ def fortran_doxygen_template(subroutine):
             
     template += '!!\n'
     return template
+
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from email.message import EmailMessage
+import base64
+
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.compose',
+    'https://www.googleapis.com/auth/gmail.modify',
+]
+def gmail_send_message(subject, body, receiver='uomeercat@gmail.com', sender='uomeercat@gmail.com'):
+    """Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            raise Exception('invalid credentials. Please run quickstart.py again.')
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        message = EmailMessage()
+
+        message.set_content(body)
+
+        message['To'] = receiver
+        message['From'] = sender
+        message['Subject'] = subject
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+        create_message = {
+            'raw': encoded_message
+        }
+        # pylint: disable=E1101
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+    return send_message
+
+
+if __name__ == '__main__':
+    gmail_send_message(subject='This is an email test', body='Kindly ignore this message', receiver='jpfloresd.97@gmail.com')
