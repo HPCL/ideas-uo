56f8e08874bb2da29ef3333d51e566e902211271
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-09-19 14:40:59 -0700

    Add tereminal authentication

--- meercat/dashboard/quickstart.py
+++ meercat/dashboard/quickstart.py
@@ -5,8 +5,6 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = [
@@ -31,29 +29,18 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES, redirect_uri='http://127.0.0.1:8000/')
-            creds = flow.run_local_server(host='127.0.0.1', port=8080)
+                'client_secret.json', 
+                SCOPES, 
+                redirect_uri='http://127.0.0.1:8080/')
+            auth_uri = flow.authorization_url()
+            print('please visit the following url to authorize:', auth_uri)
+            code = input('Enter the authorization code (found in the uri after authorizing the application): ')
+            flow.fetch_token(code=code)
+            creds = flow.credentials
+            # creds = flow.run_local_server(host='127.0.0.1', port=8080)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
-
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
-
 
 if __name__ == '__main__':
     main()
