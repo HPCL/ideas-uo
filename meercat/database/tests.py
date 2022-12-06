bc89501809526d2c301921420046d3363e76164b
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-12-06 11:35:35 -0800

    Add Doxygen test cases

--- meercat/database/tests.py
+++ meercat/database/tests.py
@@ -1,10 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-class TestTest(TestCase):
-
-    def test_passing_test(self):
-        self.assertIs(True, True)
-
-    def test_failing_test(self):
-        self.assertIs(False, True)
